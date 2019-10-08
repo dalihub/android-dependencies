8adff69acb44bc6c373e992655d12ff3316098b9
Anton Obzhirov <a.obzhirov@samsung.com>
2019-10-08 18:43:08 +0100

    Remove export ANDROID_NDK and ANDROID_SDK from build script.

--- env.sh
+++ env.sh
@@ -3,9 +3,6 @@
 #   https://dl.google.com/android/repository/cmake-3.10.2-linux-x86_64.zip
 #   https://dl.google.com/android/repository/lldb-3.1.4508709-linux-x86_64.zip
 # instructions from https://developer.android.com/ndk/guides/other_build_systems
-export NDK=$HOME/Android/Sdk/ndk-bundle
-export ANDROID_NDK=$NDK
-export ANDROID_SDK=$HOME/Android/Sdk
 export HOST_TAG=linux-x86_64
 export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
 export AR=$TOOLCHAIN/bin/aarch64-linux-android-ar
